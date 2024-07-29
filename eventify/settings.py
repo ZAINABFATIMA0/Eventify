37e1bed45e143d239668057ed6d110d197d92dd3
Zainab Fatima <zfj959@gmail.com>
2024-07-29 12:04:01 +0500

    Added filters for events listing

--- eventify/settings.py
+++ eventify/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "events",
     "communications",
     "django_extensions",
+    "django_filters",
 ]
 
 MIDDLEWARE = [
@@ -137,6 +138,9 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
     ],
 }
 
