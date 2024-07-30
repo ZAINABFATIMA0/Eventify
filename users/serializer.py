34fb0dc564121a686ef4542ce9cd126ca2fe0b1a
Zainab Fatima <zfj959@gmail.com>
2024-07-30 19:41:17 +0500

    Changes in serialzers and views

--- users/serializer.py
+++ users/serializer.py
@@ -1,5 +1,5 @@
 import random
-from datetime import datetime, timedelta
+from datetime import timedelta
 
 from rest_framework import serializers
 from django.contrib.auth.password_validation import validate_password
