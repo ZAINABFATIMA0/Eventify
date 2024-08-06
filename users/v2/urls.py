bad2be52799a946d706a159496a1e9119ae5d40c
Zainab Fatima <zfj959@gmail.com>
2024-08-06 16:34:45 +0500

    Added function based views for users app

--- users/v2/urls.py
+++ users/v2/urls.py
@@ -1,3 +1,12 @@
 from django.urls import path
+from rest_framework_simplejwt import views as jwt_views
 
-urlpatterns = []
+from .views import RegisterView, EventListingView, VerifiedRegistrationsView
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token-refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('all-events/', EventListingView.as_view(), name='event_listing'),
+    path('<int:event_id>/dashboard/', VerifiedRegistrationsView.as_view(), name='verified_registrations'),
+]
