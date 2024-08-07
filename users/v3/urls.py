9e5b4d5904df55a9f15e49672f338dfb47c05da1
Zainab Fatima <zfj959@gmail.com>
2024-08-08 02:43:59 +0500

    Generic views for users app

--- users/v3/urls.py
+++ users/v3/urls.py
@@ -1,13 +1,9 @@
 from django.urls import path
-from rest_framework_simplejwt import views as jwt_views
 
 from .views import RegistrationAPIView, EventListingAPIView, EventDashboardAPIView
 
-
 urlpatterns = [
     path('register/', RegistrationAPIView.as_view(), name='user_register'),
-    path('login/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token-refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('all-events/', EventListingAPIView.as_view(), name='events'),
     path('<int:pk>/dashboard/', EventDashboardAPIView.as_view(), name='dashbaord')
 ]
